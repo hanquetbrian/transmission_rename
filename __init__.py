cb57c5ee78068dfe0beb69831c2fd79df11dfb06
brian <he201459@students.ephec.be>
2020-02-02 22:50:51 +0100

    add get info function

--- __init__.py
+++ __init__.py
@@ -13,7 +13,16 @@ if __name__ == '__main__':
 
     data = rpc.get_torrent(1)
     # print(data)
-    c = core.Core(rpc, mediaInfo.MediaInfo())
-    c.move_media(1)
+
+    path = data[0]['downloadDir'] + '/' + data[0]['files'][0]['name']
+    print(path)
+
+    # c = core.Core(rpc, mediaInfo.MediaInfo())
+    # c.move_media(1)
+
+    media = mediaInfo.MediaInfo()
+    # media.getinfo('Mr.Robot.S04E11.eXit.1080p.10bit.AMZN.WEB-DL.AAC5.1.HEVC-Vyndros')
+    media.getinfo('Guardians.of the Galaxy Vol 2.2017.1080p.BluRay.x264-SPARKS[EtHD].mkv')
+
 
     #print(data.json()['arguments']['torrents'][1]['files'][1]['name'])
--- mediaInfo.py
+++ mediaInfo.py
@@ -1,3 +1,33 @@
+import re
+import os
+
 class MediaInfo:
     def __init__(self):
         pass
+
+    def getinfo(self, file_name):
+        media = MediaRespond()
+
+        if re.search(r'[.\s]s[0-9]{1,2}E[0-9]{1,2}', file_name, re.IGNORECASE):
+            media.isTvShow = True
+        elif os.path.splitext(file_name)[1] in ['.mkv', '.avi', '.mov', '.mpg', '.mp4']:
+            media.isMovie = True
+
+
+        part = re.split("\s|\.", file_name)
+        # print(part)
+        return media
+
+
+class MediaRespond:
+    def __init__(self):
+        self.title = ''
+        self.year = ''
+        self.isMovie = False
+        self.isTvShow = False
+        self.season = 0
+        self.episode = 0
+        self.resolution = 0
+        self.codec = []
+        self.language = ''
+        self.extra = ''
