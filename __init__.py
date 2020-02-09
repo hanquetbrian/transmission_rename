8fc61bb069dcb62a71d7921ab0e72da55c4a5a90
brian <he201459@students.ephec.be>
2020-02-09 22:56:32 +0100

    function to get info like year, season, resolution, ...

--- __init__.py
+++ __init__.py
@@ -15,14 +15,22 @@ if __name__ == '__main__':
     # print(data)
 
     path = data[0]['downloadDir'] + '/' + data[0]['files'][0]['name']
-    print(path)
+    # print(path)
 
     # c = core.Core(rpc, mediaInfo.MediaInfo())
     # c.move_media(1)
 
-    media = mediaInfo.MediaInfo()
-    # media.getinfo('Mr.Robot.S04E11.eXit.1080p.10bit.AMZN.WEB-DL.AAC5.1.HEVC-Vyndros')
-    media.getinfo('Guardians.of the Galaxy Vol 2.2017.1080p.BluRay.x264-SPARKS[EtHD].mkv')
+    media = mediaInfo
+    # m = media.getinfo('Mr.Robot.S04E11.eXit.1080p.10bit.AMZN.WEB-DL.AAC5.1.HEVC-Vyndros.avi')
+    m = media.getinfo('Thor.Ragnarok.2017.4K.HDR.2160p.Engx265.mkv')
+    # m = media.getinfo('www.movcr.tv.-The.flash.2014.S05E22.720p.HDTV.x264AAC-GUN.mkv')
+    # m = media.getinfo('Guardians.of the Galaxy Vol 2.2017.1080p.BluRay.x264-SPARKS[EtHD].mkv')
+    if m:
+        print(m)
+
+        print(m.get_file_name())
+    else:
+        print('please enter a correct file')
 
 
     #print(data.json()['arguments']['torrents'][1]['files'][1]['name'])
--- mediaInfo.py
+++ mediaInfo.py
@@ -1,22 +1,103 @@
 import re
-import os
+import datetime
 
-class MediaInfo:
-    def __init__(self):
-        pass
-
-    def getinfo(self, file_name):
-        media = MediaRespond()
-
-        if re.search(r'[.\s]s[0-9]{1,2}E[0-9]{1,2}', file_name, re.IGNORECASE):
-            media.isTvShow = True
-        elif os.path.splitext(file_name)[1] in ['.mkv', '.avi', '.mov', '.mpg', '.mp4']:
-            media.isMovie = True
+__AVAILABLE_RES = ['2160p', '4k', '1080p', '1080i', '720p', '576p', '576i', '480p', '480i']
+__AVAILABLE_CODEC = ['x264', 'h264', 'h265', 'x265', 'HEVC', 'AAC']
+__AVAILABLE_EXTENSION = ['mkv', 'avi', 'mov', 'mpg', 'mp4']
+__INFO_TO_KEEP = ['BluRay', 'UHD', 'HDTV', 'HDR', '10bit', '8bit']
 
 
-        part = re.split("\s|\.", file_name)
-        # print(part)
-        return media
+def getinfo(file_name):
+    """
+    Get the info of a media from a file name
+    """
+    print(file_name)
+    extension = file_name.split('.')[-1]
+    if extension not in __AVAILABLE_EXTENSION:
+        return False
+
+    file_name_parts = _get_filename_parts(file_name)
+    print(file_name_parts)
+    media = MediaRespond()
+    media.extension = extension
+    file_name_parts.remove(extension)
+
+    # get the year of the media
+    year_info = re.findall(r'(.*?)\|?(2[0-9]{3})', '|'.join(file_name_parts), re.IGNORECASE)
+    if year_info:
+        for year in year_info:
+            if int(year[1]) <= datetime.datetime.now().year:
+                file_name_parts.remove(year[1])
+                media.year = int(year[1])
+                media.title = _format_title(year[0])
+                for title_part in year[0].split('|'):
+                    file_name_parts.remove(title_part)
+
+    # get the season and episode of the tv show
+    episode = re.search(r'(.*?)\|?s([0-9]{1,2})E([0-9]{1,2})', '|'.join(file_name_parts), re.IGNORECASE)
+
+    if episode:
+        media.isTvShow = True
+        if not media.title:
+            media.title = _format_title(episode.group(1))
+        media.season = episode.group(2)
+        media.episode = episode.group(3)
+        for part in episode.group(0).split('|'):
+            file_name_parts.remove(part)
+        print(file_name_parts)
+    else:
+        media.isMovie = True
+
+    for part in list(file_name_parts):
+        res = [ele for ele in __AVAILABLE_RES if ele.lower() in part.lower()]
+        codec = [ele for ele in __AVAILABLE_CODEC if ele.lower() in part.lower()]
+        extra = [t for t in __INFO_TO_KEEP if t.lower() in part.lower()]
+
+        if res:
+            media.resolution.append(res[0])
+            file_name_parts.remove(part)
+        if codec:
+            media.codec.append(codec[0])
+            file_name_parts.remove(part)
+        if extra:
+            media.extra.append(extra[0])
+            file_name_parts.remove(part)
+
+    # print(file_name_parts)
+
+    return media
+
+
+def _format_title(name):
+    """
+    return a title in the correct format
+    """
+    new_name = name.lower().split('|')
+    new_name = [s.capitalize() for s in new_name]
+    new_name = ' '.join(new_name)
+
+    return new_name
+
+
+def _get_filename_parts(name):
+    """
+    Filter what we don't want in the filename and return the filename in array
+    """
+    new_name = re.sub(r'www\.\w+\.\w{2,4}', '', name, flags=re.IGNORECASE)
+    new_name = re.sub(r'\[.*\]', '', new_name, flags=re.IGNORECASE)
+    new_name = re.sub(r'^[.\-_*$]+', '', new_name, flags=re.IGNORECASE)
+    new_name = re.sub(r'[.\-_\s]+', '|', new_name, flags=re.IGNORECASE)
+
+    return new_name.split('|')
+
+
+def _get_language(parts_filename):
+    """
+    Get the language of the filename
+    """
+    language = ['French', 'English', 'Spanish', 'Hindi']
+
+    return language
 
 
 class MediaRespond:
@@ -27,7 +108,32 @@ class MediaRespond:
         self.isTvShow = False
         self.season = 0
         self.episode = 0
-        self.resolution = 0
+        self.resolution = []
         self.codec = []
-        self.language = ''
-        self.extra = ''
+        self.language = []
+        self.extra = []
+        self.extension = ''
+
+    def get_file_name(self):
+        new_file_name = [self.title, str(self.year)]
+        if self.isTvShow:
+            new_file_name.append('S' + str(self.season) + 'E' + str(self.episode))
+        new_file_name.append(self.language)
+        new_file_name.append('.'.join(self.resolution))
+        new_file_name.append('.'.join(self.codec))
+        new_file_name.append('.'.join(self.extra))
+        new_file_name.append(self.extension)
+        return '.'.join([name.replace(' ', '.') for name in new_file_name if name])
+
+    def __str__(self):
+        s = 'Title: ' + self.title
+        s += '\nYear: ' + str(self.year)
+        s += '\nSeason: ' + str(self.season)
+        s += '\nEpisode: ' + str(self.episode)
+        s += '\nResolution: ' + str(self.resolution)
+        s += '\nCodec: ' + str(self.codec)
+        s += '\nLanguage: ' + str(self.language)
+        s += '\nExtra: ' + str(self.extra)
+        s += '\nIs movie: ' + str(self.isMovie)
+        s += '\nIs tv show: ' + str(self.isTvShow)
+        return s
